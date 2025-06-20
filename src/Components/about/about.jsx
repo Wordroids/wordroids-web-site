6f4029e45778ca3b6dd3addf8c8ff5c6df033428
Supuni Shashikala <supuni.20231868@iit.ac.lk>
2025-06-20 12:00:16 +0530

    Added styles for about section.

--- src/Components/about/about.jsx
+++ src/Components/about/about.jsx
@@ -7,26 +7,28 @@ import checker from '../../assets/Checker.jpg'
 const About = () => {
   return (
     <section id='about'>
-     
-     <div className="container about__container">
+        <div className="container about__outer-wrapper">  {/* outer border */}
+            <div className="about__content-wrapper">        {/* brown background */}
+                <div className='about__checker'>
+                    <div className='about__checker-image'>
+                        <img src={checker} alt="About" />
+                    </div>
+                </div>
 
-         <div className='about_checker'>
-          <div className='about__checker-image'>
-            <img src={checker} alt="About Image" />
-          </div>
-        </div> 
-
-          <div className="about_intro">
+            <div className="about_intro">
                 <h4>YOUR <span className="highlight">PARTNER</span> IN DIGITAL TRANSFORMATION </h4>
                 <h1>Empowering Businesses Through Innovative Digital Solutions</h1>
-               
-                <p>We are a passionate team of innovators,creators, and problem-solvers dedicated to empowerind businesses with cutting-edge digital solutions.With a focus on quality,creativity and functionality, we aim to transform your ideas into impactful realities.</p>
-          </div>
-           
+                <p>
+                We are a passionate team of innovators, creators, and problem-solvers dedicated to empowering businesses with cutting-edge digital solutions. With a focus on quality, creativity and functionality, we aim to transform your ideas into impactful realities.
+                </p>
+            </div>
         </div>
-      
-    </section>
+    </div>
+</section>
   )
 }
 
 export default About
+
+
+

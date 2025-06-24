0687ac14a0ca87125339fdf739537cc74a1f64b3
Supuni Shashikala <supuni.20231868@iit.ac.lk>
2025-06-24 11:01:29 +0530

    Created the introduction part technology section

---
+++ src/Components/technology/technology.jsx
@@ -0,0 +1,40 @@
+// import React from "react";
+// import './technology.css'
+
+
+// const Technology = () => {
+//     return(
+//         <div>Technology</div>
+//     )
+// }
+
+// export default Technology
+
+
+
+import React from 'react'
+import './technology.css'
+
+
+
+
+const Technology = () => {
+  return (
+    <section id='technology'>
+        
+
+            <div className="technology_intro">
+                <h4>EXPLORE OUR <span className="highlight">TECHNOLOGICAL</span> EXPERTISE</h4>
+                <h1>Harnessing the Power of Advanced Technology to Drive Transformation and Growth</h1>
+                <p>
+                 We use the latest technologies to create innovative solutions that boost efficiency, enhance user experience, and help businesses stay ahead in a rapidly evolving digital landscape
+                </p>
+            </div> 
+  
+    </section>
+  )
+}
+
+export default Technology
+
+

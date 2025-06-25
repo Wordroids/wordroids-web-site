1c16b1ac0c95014e702454ccb712c98931810a43
Supuni Shashikala <supuni.20231868@iit.ac.lk>
2025-06-25 14:54:06 +0530

    modify the header section.

--- src/Components/header/header.jsx
+++ src/Components/header/header.jsx
@@ -1,38 +1,42 @@
 import React from 'react'
 import './header.css'
 import Businessman from '../../assets/Businessman.png'
+import { GiJetpack } from "react-icons/gi";
 
-
-
-const header = () => {
+const Header = () => {
   return (
     <header>
-      <div className = "container header_container">
+      {/* Orange abstract shape */}
+      <div className="hero-bg-shape"></div>
 
-        <div class = "header_text">
-        
-        <h1>Crafting Digital</h1>
-        <h1>Masterpiece</h1>
-        <p>From visinory web solutions to stunning designs, wordroids <br></br>transforms your digital dreams in to reality.</p>
-        <h4>Let's innovate together!</h4>
-        
+      <div className="container header_container">
+        <div className="header_text">
+          <h1>Crafting Digital</h1>
+          <h1>Masterpieces</h1>
+          <p>
+            From visionary web solutions to stunning designs, Wordroids <br />
+            transforms your digital dreams into reality.
+          </p>
+          <h4>Let's innovate together!</h4>
+          <div className='welcome_button'>
+            <a href="#contact" className='btn btn-primary'>GET STARTED TODAY!</a>
+          </div>
         </div>
-        
 
         <div className="man">
           <img src={Businessman} alt="man" />
-        </div> 
-
-         <div className='welcome_button'>
-           <a href= "#contact" className ='btn btn-primary'>GET STARTED TODAY!</a>
-         </div>
-      
-      
+          <div className="projects-card">
+            {/* Use the GiJetpack icon component here */}
+            <GiJetpack className="projects-icon" />
+            <span>
+              <strong>18+</strong><br />
+              Successful Projects
+            </span>
+          </div>
+        </div>
       </div>
     </header>
   )
 }
 
-export default header
-
-
+export default Header

26236bfbaa4d17e0507f74a7545f790dc9e6d16f
Supuni Shashikala <supuni.20231868@iit.ac.lk>
2025-06-30 13:21:54 +0530

    added background style for project section

--- src/Components/projects/projects.jsx
+++ src/Components/projects/projects.jsx
@@ -3,9 +3,8 @@ import './projects.css';
 import IMG1 from '../../assets/project1.png';
 import IMG2 from '../../assets/project2.png';
 import IMG3 from '../../assets/project3.png';
-import { GiOpenBook } from "react-icons/gi"
-
-//DO NOT USE IMAGE IN PRODUCTION
+import { GiOpenBook } from "react-icons/gi";
+import wave3 from '../../assets/wave3.png';
 
 const data = [
   {
@@ -13,92 +12,78 @@ const data = [
     image: IMG1,
     title: 'Car Smash Repair Website',
     position1: 'Wordpress Development',
-    // position2: 'UI/UX Designing',
-    // position3: 'Web Hosting'
   },
   {
     id: 2,
     image: IMG2,
     title: 'Car Care Melbourne Website',
     position1: 'UI/UX',
-    // position2: 'UI/UX Designing',
-    // position3: 'Web Hosting'
-
   },
   {
     id: 3,
     image: IMG3,
     title: 'Import Revolution Website',
     position1: 'UI/UX',
-    // position2: 'UI/UX Designing',
-    // position3: 'Web Hosting'
   }
-]
+];
 
-const projects = () => {
+const Projects = () => {
   return (
-    <section id='projects'>
-               <div className="project_header">
-                    <div className="project_intro">
-                        <h4>EXPLORE OUR<span className="highlight"> PORTFOLIO</span> OF INNOVATIVE PROJECTS</h4>
-                        <h1>Building Impactful Solutions <br />for businesses Worldwide</h1>
-                    </div>
+    <section id="projects">
+      <div className="project_background">
+        <img src={wave3} alt="wave background" className="wave3-bg" />
 
-                    <div className="project_para">
-                        <p>
-                        Explore our diverse portfolio of successful projects showcasing innovation,
-                        creativity and client-focused solutions. Each project reflects our dedication
-                        to delivering impactful results tailored to unique business needs.
-                        </p>
-                    </div>
-               </div>
+        <div className="project_overlay">
+          {/* Header content */}
+          <div className="project_header">
+            <div className="project_intro">
+              <h4>
+                EXPLORE OUR<span className="highlight"> PORTFOLIO</span> OF INNOVATIVE PROJECTS
+              </h4>
+              <h1>
+                Building Impactful Solutions <br />for businesses Worldwide
+              </h1>
+            </div>
+            <div className="project_para">
+              <p>
+                Explore our diverse portfolio of successful projects showcasing innovation,
+                creativity and client-focused solutions. Each project reflects our dedication
+                to delivering impactful results tailored to unique business needs.
+              </p>
+            </div>
+          </div>
 
-      <div className='container project__container'>
-        {
-          data.map(({id, image,title,position1}) => {
-            return(
-              <article key={id} className='project__item'>
-                <div className='project__item-image'>
-                  <img src={image} alt={title}/>
+          {/* Project cards */}
+          <div className="container project__container">
+            {data.map(({ id, image, title, position1 }) => (
+              <article key={id} className="project__item">
+                <div className="project__item-image">
+                  <img src={image} alt={title} />
                 </div>
-
                 <h3>{title}</h3>
-
-               <div className='project__item-cta'>
-                 <span className='project__role'>Wordpress Development</span>
-               </div>
-
-               {/* <div className='project__item-cta'>
-                <span className='project__role'>UI/UX Designing</span>
-               </div>
-
-                <div className='project__item-cta'>
-                <span className='project__role'>Web Hosting</span>
-                </div> */}
-
-
-
+                <div className="project__item-cta">
+                  <span className="project__role">{position1}</span>
+                </div>
               </article>
-            )
-          })
-        }
-       
+            ))}
+          </div>
 
-     </div>
-
-     <div className="browse_button_wrapper3" >
-              <button className="browse_button3"onClick={() => {
-                       // Example: scroll to services or navigate
-                       document.getElementById('projects')?.scrollIntoView({ behavior: 'smooth' })
-                     }}>
-                  <GiOpenBook className="browse_icon3" />     
-                 REVIEW PROJECTS
-              </button>
+          {/* Browse button */}
+          <div className="browse_button_wrapper3">
+            <button
+              className="browse_button3"
+              onClick={() => {
+                document.getElementById('projects')?.scrollIntoView({ behavior: 'smooth' });
+              }}
+            >
+              <GiOpenBook className="browse_icon3" />
+              REVIEW PROJECTS
+            </button>
+          </div>
+        </div>
       </div>
-
-
     </section>
   );
 };
 
-export default projects;
+export default Projects;
